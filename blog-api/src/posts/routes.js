f11efe277d181c3c8b679d09e37cd005a62c70ae
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-09-13 18:45:49 +0100

    chore: created swagger docs

--- blog-api/src/posts/routes.js
+++ blog-api/src/posts/routes.js
@@ -2,7 +2,40 @@ const router = require('express').Router();
 const auth = require('../auth/middleware');
 const { getPosts, getMyPosts, createPost } = require('./controllers');
 
-
+/**
+* @swagger
+* /api/posts:
+*  get:
+*    summary: Get all Posts
+*    tags: [Posts]
+*    responses:
+*      200:
+*        description: Successful response
+*/
 router.get('/', getPosts);
+
+/**
+ * @swagger
+ * /api/my-posts:
+ *   get:
+ *     summary: Get all Posts made by Logged in user
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: Successful Response
+ */
 router.get('/my-posts', auth, getMyPosts);
+
+/**
+ * @swagger
+ * /api/posts:
+ *   post:
+ *     summary: Create Post
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ */
 router.post('/', auth, createPost);
+
+module.exports = router;
