8b48fc80461707e13342556327b879e2eaabb5ce
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-09-13 13:56:44 +0100

    Create controllers.js

---
+++ blog-api/src/auth/controllers.js
@@ -0,0 +1,49 @@
+const User = require('../users/models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            username, email, password: hashed
+        });
+
+        res.status(201).json({
+            message: 'User registered',
+            user
+        })
+    } catch (error) {
+        res.status(400).json({ 'error': error.message });
+    }
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(404).json({ 'message': 'User not found' });
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) return res.status(401).json({ message: 'Invalid Credentials' });
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            {expiresIn: '1d'}
+        )
+
+        res.json({ token })
+        
+    } catch (error) {
+        res.status(400).json({ 'error': error.message });
+    }
+}
+
+
+module.exports = { register, login }
