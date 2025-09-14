fafe1a82fdb2c041cb5dd72bc5021edc359f6aa4
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-09-14 10:47:22 +0100

    update: changed key and error message

--- blog-api/src/auth/middleware.js
+++ blog-api/src/auth/middleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 // Authorization Check
 module.exports = (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'No token' });
+    if (!token) return res.status(401).json({ error: 'Missing Authorization token' });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
