9a5bd275bdee457362bb87aa841a992d878acca9
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-09-14 11:04:45 +0100

    update: added validation for post body creation

--- blog-api/src/posts/controllers.js
+++ blog-api/src/posts/controllers.js
@@ -1,6 +1,23 @@
+const Joi = require('joi');
 const Post = require('./models');
 
+
+const post_schemas = Joi.object({
+    title: Joi.string().min(6).required(),
+    body: Joi.string().min(10).required(),
+})
+
 const createPost = async (req, res) => {
+    const { error } = post_schemas.validate(req.body);
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: 'title or body cannot be empty' });
+    }
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message })
+    }
+
     try {
         const post = await Post.create({
             ...req.body, author: req.user
